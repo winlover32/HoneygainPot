da8fca0e2b1a56abcd861987dddbe0c4e4fff3bc
GorouFlex <98001973+gorouflex@users.noreply.github.com>
2023-12-22 21:57:50 +0700

    fixes JPT mode
    tks to HoneygainAutoClaim by MrLolf

--- main.py
+++ main.py
@@ -224,13 +224,13 @@ def achievements_claim(s: requests.session, header: dict[str, str]) -> bool:
         return False
     for achievement in achievements['data']:
         if (not achievement['is_claimed'] and 'progresses' in achievement and
-                not len(achievement['progresses'][0]) > 0):
+                achievement['progresses'] == []):
             s.post(urls['achievement_claim'],
                    json={"user_achievement_id": achievement['id']},
                    headers=header)
             print(f"{colors.WARNING}Trying to claim {achievement['title']}{colors.ENDC}")
         elif (not achievement['is_claimed'] and 'progresses' in achievement and
-              len(achievement['progresses'][0]) > 0 and
+              not achievement['progresses'] == [] and
               achievement['progresses'][0]['current_progress'] ==
               achievement['progresses'][0]['total_progress']):
             s.post(urls['achievement_claim'],
