a5cd7490e1d54959a50fc7218d3629be0c9eb644
GorouFlex <98001973+gorouflex@users.noreply.github.com>
2024-01-25 11:58:38 +0700

    remove useless line

--- main.py
+++ main.py
@@ -34,7 +34,6 @@ is_jwt = config.get('User', 'IsJWT', fallback='0')
 if os.getenv('GITHUB_ACTIONS') == 'true':
     print(f"{colors.OKBLUE}Powered by GitHub Actions V3 and Python{colors.ENDC}")
     print(f"{colors.OKGREEN}Run with GitHub Actions: Yes{colors.ENDC}")
-    print(f"{colors.WHITE}Original repo: gorouflex/HoneygainPot{colors.ENDC}")
     print(f"{colors.WHITE}Current forked repo: {os.getenv('GITHUB_REPOSITORY')}{colors.ENDC}")
     user_repo = os.getenv('GITHUB_REPOSITORY')
     ORIGINAL_REPO = 'gorouflex/HoneygainPot'
